99ba83b09612ead7777ebeb4a3c71f2181c34030
Daffa Hanifisyafiq <dandroid36@gmail.com>
2025-08-23 01:02:23 +0700

    feat: add initial home page with image and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,38 @@
 import Navbar from '@/components/Navbar'
+import Image from 'next/image'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <main className={`pt-[100px]`}>
-        <section>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, deserunt
-          inventore, veniam, iusto et delectus blanditiis numquam officiis
-          magnam impedit expedita saepe ipsum mollitia asperiores nam! Provident
-          hic sunt error!
+      <main className='bg-ivory px-32 pt-[100px]'>
+        <section id='home' className='flex w-full justify-between'>
+          <div className='relative h-[280px] w-[480px]'>
+            <Image
+              src='/images/blob-header.svg'
+              alt='blob-header'
+              width={480}
+              height={280}
+              className='absolute -z-[1px]'
+            />
+            <div className='absolute top-24 left-12'>
+              <p className='text-coffee text-4xl font-semibold'>
+                KOS IBU AMI PETARUKAN
+              </p>
+            </div>
+            <div className='absolute top-64 left-12'>
+              <p className='text-coffee text-base font-semibold'>
+                Jalan Perwira No.51 Kec. Petarukan <br /> Kab. Pemalang
+              </p>
+            </div>
+          </div>
+          <Image
+            src='/images/halaman-depan-kos.webp'
+            alt='halaman-depan-kos'
+            width={480}
+            height={484}
+            className='rounded-bl-[60px]'
+          />
         </section>
       </main>
     </>
